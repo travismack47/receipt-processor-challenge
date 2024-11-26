72b7f587ef0bb632f6cf075c22ebf206b769b8ba
travismack47 <travismack47@protonmail.com>
2024-11-25 20:38:50 -0600

    installed dayjs, refactored date and time logic in calculatePoints function to use dayjs

--- src/points.js
+++ src/points.js
@@ -1,3 +1,5 @@
+const dayjs = require('dayjs');
+
 function calculatePoints(receipt) {
     let points = 0;
 
@@ -17,22 +19,24 @@ function calculatePoints(receipt) {
         const descriptionLength = item.shortDescription.trim().length;
         if (descriptionLength % 3 === 0) {
             itemPoints += Math.ceil(parseFloat(item.price) * 0.2);
-        }
+        };
     });
+    
     points += itemPoints;
 
-    const day = parseInt(receipt.purchaseDate.split('-')[2], 10);
+    const date = dayjs(receipt.purchaseDate);
+    const day = date.date();
     if (day % 2 !== 0) points += 6;
 
-    const [hour, minute] = receipt.purchaseTime.split(':').map(Number);
-    const timeInMinutes = hour * 60 + minute;
-    const twoPMInMinutes = 14 * 60;
-    const fourPMInMinutes = 16 * 60; 
+    const time = dayjs(`${receipt.purchaseDate} ${receipt.purchaseTime}`);
+    const twoPM = dayjs(`${receipt.purchaseDate} 14:00`);
+    const fourPM = dayjs(`${receipt.purchaseDate} 16:00`);
 
-    if (timeInMinutes > twoPMInMinutes && timeInMinutes < fourPMInMinutes) {
+    if (time.isAfter(twoPM) && time.isBefore(fourPM)) {
         points += 10;
-    }
+    };
+
     return points;
-}
+};
 
 module.exports = calculatePoints;
