5796eb17ea56f4a1e73c61632a81329efa9c424f
travismack47 <travismack47@protonmail.com>
2024-11-26 22:36:06 -0600

    removed dotenv dependency, installed express-rate-limit, set up rate limiting, created rate limiting tests

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const calculatePoints = require('./points.js');
-const { saveReceipt, getReceiptPoints } = require('./storage.js');
-const receiptSchema = require('./schema.js');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const calculatePoints = require("./points.js");
+const { saveReceipt, getReceiptPoints } = require("./storage.js");
+const receiptSchema = require("./schema.js");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(helmet());
 
 const PORT = process.env.PORT || 3000;
 
-app.post('/receipts/process', async (req, res, next) => {
+const maxGlobalRequests = 100;
+const maxReceiptRequests = 100;
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: maxGlobalRequests,
+  message: { error: "Too many requests, please try again later." },
+});
+
+app.use(globalLimiter);
+
+const receiptLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: maxReceiptRequests,
+  message: {
+    error: "Too many requests to /receipts/process, please try again later.",
+  },
+});
+
+app.post("/receipts/process", receiptLimiter, async (req, res, next) => {
   try {
     const { error, value: receipt } = receiptSchema.validate(req.body);
 
     if (error) {
-      const validationError = new Error(`Invalid receipt data: ${error.details[0].message}`);
+      const validationError = new Error(
+        `Invalid receipt data: ${error.details[0].message}`
+      );
       validationError.status = 400;
       throw validationError;
     }
@@ -36,13 +57,13 @@ app.post('/receipts/process', async (req, res, next) => {
   }
 });
 
-app.get('/receipts/:id/points', async (req, res, next) => {
+app.get("/receipts/:id/points", receiptLimiter, async (req, res, next) => {
   try {
     const id = req.params.id;
     const points = getReceiptPoints(id);
 
     if (points === null) {
-      const error = new Error('Receipt not found');
+      const error = new Error("Receipt not found");
       error.status = 404;
       throw error;
     }
@@ -54,18 +75,20 @@ app.get('/receipts/:id/points', async (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'Resource not found' });
-  });
+  res.status(404).json({ error: "Resource not found" });
+});
 
-  app.use((err, req, res, next) => {
-    console.error(`Error occurred during request to ${req.path}`);
-    console.error(err.stack);
-  
-    res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
-  });  
+app.use((err, req, res, next) => {
+  console.error(`Error occurred during request to ${req.path}`);
+  console.error(err.stack);
+
+  res
+    .status(err.status || 500)
+    .json({ error: err.message || "Internal Server Error" });
+});
 
 app.listen(PORT, () => {
-  console.log('Server running on PORT', PORT);
+  console.log("Server running on PORT", PORT);
 });
 
 module.exports = app;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,185 +1,231 @@
-const request = require('supertest');
-const app = require('../src/index');
+const request = require("supertest");
+const app = require("../src/index");
+let maxReceiptRequests = 20;
 
-describe('Receipt Processor API', () => {
-    it('should process a receipt and return an ID', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "Target",
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
+describe("Receipt Processor API", () => {
+  it("should process a receipt and return an ID", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: "Target",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('id');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("should return points for a valid receipt ID", async () => {
+    const processResponse = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: "Target",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
+
+    const id = processResponse.body.id;
+
+    const pointsResponse = await request(app).get(`/receipts/${id}/points`);
+    expect(pointsResponse.statusCode).toBe(200);
+    expect(pointsResponse.body).toHaveProperty("points");
+  });
+
+  it("should return 400 Bad Request when required fields are missing", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("should return 400 Bad Request when data types are invalid", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: 12345,
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("should return 400 Bad Request when date format is invalid", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: "Target",
+        purchaseDate: "01-01-2022",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("should return 400 Bad Request when time format is invalid", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: "Target",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "25:61",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("should return 400 Bad Request when items array is empty", async () => {
+    const response = await request(app).post("/receipts/process").send({
+      retailer: "Target",
+      purchaseDate: "2022-01-01",
+      purchaseTime: "13:01",
+      items: [],
+      total: "1.00",
     });
 
-    it('should return points for a valid receipt ID', async () => {
-        const processResponse = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "Target",
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
 
-        const id = processResponse.body.id;
+  it("should return 404 Not Found for a non-existent receipt ID", async () => {
+    const response = await request(app).get("/receipts/nonexistent-id/points");
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toHaveProperty("error", "Receipt not found");
+  });
 
-        const pointsResponse = await request(app).get(`/receipts/${id}/points`);
-        expect(pointsResponse.statusCode).toBe(200);
-        expect(pointsResponse.body).toHaveProperty('points');
+  it("should calculate the correct points for a sample receipt", async () => {
+    const receipt = {
+      retailer: "Target",
+      purchaseDate: "2022-01-01",
+      purchaseTime: "13:01",
+      items: [
+        { shortDescription: "Mountain Dew 12PK", price: "6.49" },
+        { shortDescription: "Emils Cheese Pizza", price: "12.25" },
+        { shortDescription: "Knorr Creamy Chicken", price: "1.26" },
+        { shortDescription: "Doritos Nacho Cheese", price: "3.35" },
+        { shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", price: "12.00" },
+      ],
+      total: "35.35",
+    };
+
+    const expectedPoints = 28;
+
+    const processResponse = await request(app)
+      .post("/receipts/process")
+      .send(receipt);
+
+    const id = processResponse.body.id;
+
+    const pointsResponse = await request(app).get(`/receipts/${id}/points`);
+    expect(pointsResponse.statusCode).toBe(200);
+    expect(pointsResponse.body).toHaveProperty("points", expectedPoints);
+  });
+
+  it("should sanitize input to prevent XSS attacks", async () => {
+    const response = await request(app)
+      .post("/receipts/process")
+      .send({
+        retailer: "<script>alert('XSS')</script>",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [
+          {
+            shortDescription: "<img src=x onerror=alert('XSS')>",
+            price: "1.00",
+          },
+        ],
+        total: "1.00",
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  describe("Receipt Processor API Rate Limiting", () => {
+    it("should enforce rate limiting and return 429 after exceeding the limit", async () => {
+      const receipt = {
+        retailer: "Rate Limit Test",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      };
+
+      const promises = [];
+
+      for (let i = 0; i < 20; i++) {
+        promises.push(
+          request(app)
+            .post("/receipts/process")
+            .set("X-Forwarded-For", "123.123.123.123")
+            .send(receipt)
+        );
+      }
+
+      const responses = await Promise.all(promises);
+
+      const successfulRequests = responses.filter(
+        (response) => response.statusCode === 200
+      ).length;
+      const tooManyRequestsCount = responses.filter(
+        (response) => response.statusCode === 429
+      ).length;
+
+      expect(successfulRequests).toBe(maxReceiptRequests);
+      expect(tooManyRequestsCount).toBe(20 - maxReceiptRequests);
     });
 
-    it('should return 400 Bad Request when required fields are missing', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
+    it("should handle multiple concurrent requests and enforce rate limits", async () => {
+      const receipt = {
+        retailer: "Concurrent Test",
+        purchaseDate: "2022-01-01",
+        purchaseTime: "13:01",
+        items: [{ shortDescription: "Item", price: "1.00" }],
+        total: "1.00",
+      };
 
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
+      const promises = [];
 
-    it('should return 400 Bad Request when data types are invalid', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: 12345, 
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
+      for (let i = 0; i < 20; i++) {
+        promises.push(
+          request(app)
+            .post("/receipts/process")
+            .set("X-Forwarded-For", "123.123.123.123")
+            .send(receipt)
+        );
+      }
 
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
+      const responses = await Promise.all(promises);
 
-    it('should return 400 Bad Request when date format is invalid', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "Target",
-                purchaseDate: "01-01-2022", 
-                purchaseTime: "13:01",
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
-
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
-
-    it('should return 400 Bad Request when time format is invalid', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "Target",
-                purchaseDate: "2022-01-01",
-                purchaseTime: "25:61", 
-                items: [{ shortDescription: "Item", price: "1.00" }],
-                total: "1.00"
-            });
-
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
-
-    it('should return 400 Bad Request when items array is empty', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "Target",
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [], 
-                total: "1.00"
-            });
-
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
-
-    it('should return 404 Not Found for a non-existent receipt ID', async () => {
-        const response = await request(app).get('/receipts/nonexistent-id/points');
-        expect(response.statusCode).toBe(404);
-        expect(response.body).toHaveProperty('error', 'Receipt not found');
-    });
-
-    it('should calculate the correct points for a sample receipt', async () => {
-        const receipt = {
-            retailer: "Target",
-            purchaseDate: "2022-01-01",
-            purchaseTime: "13:01",
-            items: [
-                { shortDescription: "Mountain Dew 12PK", price: "6.49" },
-                { shortDescription: "Emils Cheese Pizza", price: "12.25" },
-                { shortDescription: "Knorr Creamy Chicken", price: "1.26" },
-                { shortDescription: "Doritos Nacho Cheese", price: "3.35" },
-                { shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", price: "12.00" }
-            ],
-            total: "35.35"
-        };
-
-        const expectedPoints = 28; 
-
-        const processResponse = await request(app)
-            .post('/receipts/process')
-            .send(receipt);
-
-        const id = processResponse.body.id;
-
-        const pointsResponse = await request(app).get(`/receipts/${id}/points`);
-        expect(pointsResponse.statusCode).toBe(200);
-        expect(pointsResponse.body).toHaveProperty('points', expectedPoints);
-    });
-
-    it('should sanitize input to prevent XSS attacks', async () => {
-        const response = await request(app)
-            .post('/receipts/process')
-            .send({
-                retailer: "<script>alert('XSS')</script>",
-                purchaseDate: "2022-01-01",
-                purchaseTime: "13:01",
-                items: [
-                    { shortDescription: "<img src=x onerror=alert('XSS')>", price: "1.00" }
-                ],
-                total: "1.00"
-            });
-
-        expect(response.statusCode).toBe(400);
-        expect(response.body).toHaveProperty('error');
-    });
-
-    it('should handle multiple concurrent requests', async () => {
-        const receipt = {
-            retailer: "Concurrent Test",
-            purchaseDate: "2022-01-01",
-            purchaseTime: "13:01",
-            items: [{ shortDescription: "Item", price: "1.00" }],
-            total: "1.00"
-        };
-
-        const promises = [];
-
-        for (let i = 0; i < 10; i++) {
-            promises.push(request(app).post('/receipts/process').send(receipt));
+      responses.forEach((response, index) => {
+        if (index < maxReceiptRequests) {
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toHaveProperty("id");
+        } else {
+          expect(response.statusCode).toBe(429);
         }
-
-        const responses = await Promise.all(promises);
-
-        responses.forEach(response => {
-            expect(response.statusCode).toBe(200);
-            expect(response.body).toHaveProperty('id');
-        });
+      });
     });
+  });
 });
